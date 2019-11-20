50b6401c92f3d0000af144a8c392ef05c8a56854
Chamisa Edmo <chamisaedmo@yahoo.com>
2019-11-20 10:29:01 -0700

    boiler plate for the html + button

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"/>
+	<title>Hope this works</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="script.js"></script>
+</head>
+	<body>
+		<form action="" method="get">
+			<input type="text" name="location"/>
+			<button type="submit">Please?</button>
+		</form>
+	</body>
