e905ef83e8a9dcd0f81c04f01933d816e63a87c1
Chamisa Edmo <chamisaedmo@yahoo.com>
2019-11-20 13:59:36 -0700

    added js to js file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-//place holding
+//added inline js to own file
+
+$.ajax({
+	url:"https://api.nasa.gov/planetary/apod?api_key=f1NN7ANpqHGvZkORKVl5H8jvXxgAGk9jpWMOOhx0",
+	success: function(whatyougot){
+		document.getElementById("img").innerHTML="<img src='" +whatyougot.url+"'style='width:50%;'/>";
+		document.getElementById("copyright").innerHTML="<By "+whatyougot.copyright;
+		document.getElementById("title").innerHTML=whatyougot.title;
+		document.getElementById("explanation").innerText=whatyougot.explanation;
+	}
+});
