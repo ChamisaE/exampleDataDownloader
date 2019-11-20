a866cb121e43013a365bec8dcd27fe81e5d68995
Chamisa Edmo <chamisaedmo@yahoo.com>
2019-11-20 14:29:45 -0700

    got date

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@
 $.ajax({
 	url:"https://api.nasa.gov/planetary/apod?api_key=f1NN7ANpqHGvZkORKVl5H8jvXxgAGk9jpWMOOhx0",
 	success: function(whatyougot){
-		document.getElementById("img").innerHTML="<img src='" +whatyougot.url+"'style='width:50%;'/>";
-		document.getElementById("copyright").innerHTML="<By "+whatyougot.copyright;
+		document.getElementById("img").innerHTML="<img src='"+whatyougot.url+"'style='width:50%;'/>";
 		document.getElementById("title").innerHTML=whatyougot.title;
+		document.getElementById("date").innerText=whatyougot.date;
 		document.getElementById("explanation").innerText=whatyougot.explanation;
 	}
 });
